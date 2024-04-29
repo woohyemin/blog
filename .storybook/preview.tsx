e08cca723300dcfcecdadfef320f9e884cd151e7
우혜민 <woo28870826@gmail.com>
2024-04-30 03:07:29 +0900

    storybook RecoilRoot 적용

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,6 +3,8 @@ import { withThemeFromJSXProvider } from "@storybook/addon-themes";
 import GlobalStyles from "../src/styles/GlobalStyles";
 import { darkTheme, lightTheme } from "../src/styles/themeStyles";
 import { ThemeProvider } from "styled-components";
+import { RecoilRoot } from "recoil";
+import React from "react";
 
 const preview: Preview = {
   parameters: {
@@ -25,6 +27,11 @@ export const decorators = [
     Provider: ThemeProvider,
     GlobalStyles,
   }),
+  (Story) => (
+    <RecoilRoot>
+      <Story />
+    </RecoilRoot>
+  ),
 ];
 
 export default preview;
