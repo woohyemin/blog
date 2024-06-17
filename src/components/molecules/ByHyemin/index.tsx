fb642537e59ee4f8e1a3d92254e37113b711c3e4
우혜민 <woo28870826@gmail.com>
2024-06-17 14:14:05 +0900

    ByHyemin 컴포넌트 추가

---
+++ src/components/molecules/ByHyemin/index.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React from "react";
+import { Row } from "../layout/FlexBox/Row";
+import { Typography } from "@/components/atoms/data-display/Typography";
+import Link from "next/link";
+import styled from "styled-components";
+
+const StyledLink = styled(Link)`
+  text-decoration-color: ${({ theme }) => theme.palette.text.secondary};
+  text-decoration-thickness: 1px;
+`;
+
+interface ByHyeminProps {
+  variant?: "default" | "blog";
+}
+
+/**
+ * ByHyemin component
+ */
+export const ByHyemin = ({ variant = "default" }: ByHyeminProps) => {
+  return (
+    <Row gap="4px" pb="4px" alignItems="end">
+      <Typography size="h6" color="disabled">
+        by
+      </Typography>
+      {variant === "default" && (
+        <StyledLink href="/">
+          <Typography size="h6" color="secondary">
+            hyemin
+          </Typography>
+        </StyledLink>
+      )}
+      {variant === "blog" && (
+        <StyledLink href="/posts">
+          <Typography size="h6" color="secondary">
+            hyemin's blog
+          </Typography>
+        </StyledLink>
+      )}
+    </Row>
+  );
+};
