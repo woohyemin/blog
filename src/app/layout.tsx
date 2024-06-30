ae014c0b913a42f2564b8e629842ac880e001ecf
우혜민 <woo28870826@gmail.com>
2024-07-01 01:12:28 +0900

    GA 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,11 @@ import Providers from "@/styles/Providers";
 import "./globals.css";
 import { noto_sans_kr } from "./fonts";
 import { Footer } from "@/components/organisms/Footer";
+import GoogleAnalytics from "@/lib/GoogleAnalytics";
 
 const metadata: Metadata = {
-  title: "Hem Lab Blog",
-  description: "hem lab에 오신 걸 환영합니다!",
+  title: "Hyemin's blog",
+  description: "우혜민의 블로그에 오신 걸 환영합니다!",
   viewport: {
     width: "device-width",
     initialScale: 1.0,
@@ -21,6 +22,9 @@ export default function RootLayout({
   return (
     <html lang="ko" className={`${noto_sans_kr.className}`}>
       <body className="transition duration-300 bg-background min-h-screen">
+        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+        ) : null}
         <Providers>
           {children}
           <Footer />
