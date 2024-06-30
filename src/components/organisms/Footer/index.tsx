b4efef9aac4aa737b6b09ff81d497e7a11bfa5f6
우혜민 <woo28870826@gmail.com>
2024-07-01 02:04:29 +0900

    footer 정보 수정

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -8,7 +8,7 @@ export const Footer = () => {
   return (
     <footer className="w-full my-0 mx-auto px-4 max-w-[720px] pt-4 pb-8 h-20 sm:px-6 sm:pb-12 sm:h-24">
       <span className={`${montserrat.className} text-caption text-disabled`}>
-        ⓒ Hyemin Woo. All rights reserved.
+        ⓒ hem. All rights reserved.
       </span>
     </footer>
   );
