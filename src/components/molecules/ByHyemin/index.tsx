6a7476cc0b31b663e12a37e422c9c4f10b5e672f
우혜민 <woo28870826@gmail.com>
2024-07-01 02:10:31 +0900

    ByHyemin 컴포넌트 수정

--- src/components/molecules/ByHyemin/index.tsx
+++ src/components/molecules/ByHyemin/index.tsx
@@ -19,7 +19,7 @@ export const ByHyemin = ({ variant = "default" }: ByHyeminProps) => {
           className="underline decoration-underlineSecondary decoration-1 underline-offset-2"
           href="/"
         >
-          <span className="text-secondary">hyemin</span>
+          <span className="text-secondary">hem</span>
         </Link>
       )}
       {variant === "posts" && (
@@ -27,7 +27,7 @@ export const ByHyemin = ({ variant = "default" }: ByHyeminProps) => {
           className="underline decoration-underlineSecondary decoration-1 underline-offset-2"
           href="/posts"
         >
-          <span className="text-secondary">hyemin's posts</span>
+          <span className="text-secondary">hem's posts</span>
         </Link>
       )}
     </div>
