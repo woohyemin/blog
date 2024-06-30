87b7a0f707e738ea4d790ebb508cfcb82087b308
우혜민 <woo28870826@gmail.com>
2024-07-01 01:42:19 +0900

    소개 문구 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,22 +17,17 @@ export default function Home() {
       <Header
         title={
           <div className="flex flex-row gap-2 items-center">
-            <h1 className="font-sans text-primary text-h3 font-medium inline sm:text-h2">
-              우혜민
-            </h1>
-            <Dot />
             <h1
               className={`${montserrat.className} text-primary text-h4 font-medium inline sm:text-h3`}
             >
-              hyemin woo
+              hem
             </h1>
           </div>
         }
         content={
-          <div className="mt-2 sm:mt-4">
+          <div className="mt-3 sm:mt-6">
             <p className="text-secondary text-body2 sm:text-body1">
-              더 나은 UI/UX를 고민하며 개발하는 웹 프론트엔드 개발자
-              우혜민입니다.
+              더 나은 UI/UX를 고민하며 개발하는 웹 프론트엔드 개발자 hem입니다.
               <br />
               떠오른 아이디어를 가지고 스스로 구상하고 개발하는 것을 좋아하고
               경험한 것들을 기록하고 있습니다.
@@ -41,7 +36,7 @@ export default function Home() {
         }
       />
 
-      <div className="flex flex-col gap-2.5 mt-5 sm:mt-8">
+      <div className="flex flex-col gap-2.5 mt-6 sm:mt-10 sm:gap-3.5">
         <Link className="no-underline flex gap-3 items-center" href="/posts">
           <Icon icon="filledDocument" size="md" color="secondary" />
           <span className="text-body2 text-secondary underline underline-offset-2 decoration-[#b3b3b3]">
