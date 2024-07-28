8e9a65c626334e242a60ccc3c72b56873770d851
우혜민 <woo28870826@gmail.com>
2024-07-29 01:13:03 +0900

    💄 포스트 목록 섬네일 추가

--- src/components/organisms/Posts/index.tsx
+++ src/components/organisms/Posts/index.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { Post } from "@/lib/api";
 import Categories, { Category } from "@/components/Categories";
 import { Chip } from "@/components/atoms/data-display/Chip";
+import Image from "next/image";
 
 /**
  * Posts component props
@@ -38,22 +39,36 @@ export const Posts = ({ posts }: PostsProps) => {
         allPosts={posts}
       />
       {filteredPosts.length > 0 ? (
-        <div className="flex flex-col gap-4 sm:gap-6">
+        <div className="flex flex-col gap-5 sm:gap-6">
           {filteredPosts.map((el, index) => (
             <Fragment key={`${el.id}-${el.title}`}>
-              <Link href={`posts/${el.path}`} className="w-full">
-                <p className="text-caption text-disabled">{el.date}</p>
-                <h3 className="text-h5 text-primary font-medium pt-1.5 overflow-hidden whitespace-nowrap text-ellipsis sm:text-h4 sm:pt-2">
-                  {el.title}
-                </h3>
-                <p className="text-caption text-secondary pt-0.5 overflow-hidden whitespace-nowrap text-ellipsis sm:text-body2">
-                  {el.description}
-                </p>
-                <div className="flex gap-1.5 pt-2 sm:pt-3.5 sm:gap-2">
-                  {el.categories.map((category) => (
-                    <Chip key={category}>{category}</Chip>
-                  ))}
+              <Link
+                href={`posts/${el.path}`}
+                className="w-full flex flex-row justify-between gap-8"
+              >
+                <div className="flex flex-col overflow-hidden">
+                  <p className="text-caption text-disabled">{el.date}</p>
+                  <h3 className="text-h5 text-primary font-medium pt-1.5 overflow-hidden whitespace-nowrap text-ellipsis sm:text-h4 sm:pt-2">
+                    {el.title}
+                  </h3>
+                  <p className="text-caption text-secondary pt-0.5 overflow-hidden whitespace-nowrap text-ellipsis sm:text-body2">
+                    {el.description}
+                  </p>
+                  <div className="flex gap-1.5 pt-3.5 sm:pt-4 sm:gap-2">
+                    {el.categories.map((category) => (
+                      <Chip key={category}>{category}</Chip>
+                    ))}
+                  </div>
                 </div>
+                {el.thumbnail && (
+                  <Image
+                    className="w-48 min-h-full rounded-lg object-cover hidden sm:inline"
+                    src={`/images/posts/${el.thumbnail}`}
+                    alt={`${el.title} 섬네일`}
+                    width={270}
+                    height={160}
+                  />
+                )}
               </Link>
               {index < filteredPosts.length - 1 && <Divider />}
             </Fragment>
