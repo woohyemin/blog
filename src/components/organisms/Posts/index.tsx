6358e022523baec60f2157aad3b58ed5a48ae262
우혜민 <woo28870826@gmail.com>
2024-08-15 22:35:23 +0900

    💄 게시글 리스트 항목 UI 재배치

--- src/components/organisms/Posts/index.tsx
+++ src/components/organisms/Posts/index.tsx
@@ -47,18 +47,20 @@ export const Posts = ({ posts }: PostsProps) => {
                 className="w-full flex flex-row justify-between gap-8"
               >
                 <div className="flex flex-col overflow-hidden">
-                  <p className="text-caption text-disabled">{el.date}</p>
-                  <h3 className="text-h5 text-primary font-medium pt-1.5 overflow-hidden whitespace-nowrap text-ellipsis sm:text-h4 sm:pt-2">
+                  <h3 className="text-h5 text-primary font-medium overflow-hidden whitespace-nowrap text-ellipsis sm:text-h4">
                     {el.title}
                   </h3>
-                  <p className="text-caption text-secondary pt-0.5 overflow-hidden whitespace-nowrap text-ellipsis sm:text-body2">
+                  <p className="text-caption text-secondary pt-0.5 sm:pt-1 overflow-hidden whitespace-nowrap text-ellipsis sm:text-body2">
                     {el.description}
                   </p>
-                  <div className="flex gap-1 pt-3.5 sm:pt-4 sm:gap-1.5 flex-wrap">
+                  <div className="flex gap-1 pt-2 sm:pt-2.5 sm:gap-1.5 flex-wrap">
                     {el.tags.map((tag) => (
                       <Chip key={tag}>{tag}</Chip>
                     ))}
                   </div>
+                  <p className="text-caption text-disabled pt-3 sm:pt-5">
+                    {el.date}
+                  </p>
                 </div>
                 {el.thumbnail && (
                   <Image
