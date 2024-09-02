28fc84864577380852730324e641aa2ca20531d5
우혜민 <woo28870826@gmail.com>
2024-09-03 03:22:36 +0900

    💄 posts 페이지 ByHem 컴포넌트 하단 간격 조정

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -47,7 +47,9 @@ export default async function Projects() {
             <h1 className="text-primary text-h3 font-medium inline sm:text-h2">
               포스트
             </h1>
-            <ByHem />
+            <div className="pb-1">
+              <ByHem />
+            </div>
           </div>
         }
         content={
