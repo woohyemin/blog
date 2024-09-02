0e8aa6ac60f3f4012507ee05c781541bb4e6522c
우혜민 <woo28870826@gmail.com>
2024-09-03 02:24:55 +0900

    💄 Dot 컴포넌트 사이즈 조정

--- src/components/atoms/data-display/Dot/index.tsx
+++ src/components/atoms/data-display/Dot/index.tsx
@@ -6,5 +6,5 @@ import React from "react";
  * Dot component
  */
 export const Dot = () => (
-  <div className="bg-bullet w-[4px] h-[4px] rounded-[4px]" />
+  <div className="bg-bullet w-[3px] h-[3px] rounded-[3px]" />
 );
