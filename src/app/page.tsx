8755c926645078eec305bff80c3aba28f4dc379a
우혜민 <woo28870826@gmail.com>
2024-09-04 00:32:11 +0900

    💄 소개글 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,28 +42,26 @@ export default function Home() {
             <h1 className="text-primary text-h3 font-medium inline">우혜민</h1>
           </div>
         }
-        content={
-          <div className="mt-4 sm:mt-6">
-            <p className="text-secondary text-body1">
-              더 나은 사용자 경험을 고민하는 웹 프론트엔드 개발자 우혜민입니다.
-              <br />
-              떠오른 아이디어를 구현해 보는 것을 좋아하고, 경험한 것들을
-              기록하고 있습니다.
-            </p>
-
-            <div className="flex flex-row gap-x-3 gap-y-1 flex-wrap mt-4">
-              <Tag>Frontend Developer</Tag>
-              <Tag>React</Tag>
-              <Tag>Next.js</Tag>
-              <Tag>Typescript</Tag>
-              <Tag>Styled Components</Tag>
-              <Tag>Tailwind CSS</Tag>
-            </div>
-          </div>
-        }
+        content={<p className="text-secondary text-body2">프론트엔드 개발자</p>}
       />
 
-      <div className="flex flex-col gap-2.5 mt-6 sm:mt-10 sm:gap-3.5">
+      <p className="text-secondary text-body1">
+        더 나은 사용자 경험을 고민하는 프론트엔드 개발자 우혜민입니다.
+        <br />
+        떠오른 아이디어를 구현해 보는 것을 좋아하고, 경험한 것들을 기록하고
+        있습니다.
+      </p>
+
+      <div className="flex flex-row gap-x-3 gap-y-1 flex-wrap mt-3">
+        <Tag>Frontend Developer</Tag>
+        <Tag>React</Tag>
+        <Tag>Next.js</Tag>
+        <Tag>Typescript</Tag>
+        <Tag>Styled Components</Tag>
+        <Tag>Tailwind CSS</Tag>
+      </div>
+
+      <div className="flex flex-col gap-3 mt-10 sm:mt-14 sm:gap-4">
         <Link className="no-underline flex gap-3 items-center" href="/posts">
           <Icon icon="filledDocument" size="md" color="secondary" />
           <span className="text-body2 text-secondary underline underline-offset-[3px] decoration-underline">
