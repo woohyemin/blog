2ec3285ff7479f96630ef5e19928b0ece13b4ac0
우혜민 <woo28870826@gmail.com>
2024-10-01 23:16:35 +0900

    💄 DescriptionBox overflow: auto 추가

--- src/components/ui/DescriptionBox.tsx
+++ src/components/ui/DescriptionBox.tsx
@@ -20,7 +20,7 @@ export const DescriptionBox = ({
   content,
 }: PropsWithChildren<DescriptionBoxProps>) => {
   return (
-    <div className="flex bg-paper rounded-lg p-3 mt-2 mb-3 gap-2 sm:mt-3 sm:mb-5 sm:p-4">
+    <div className="flex bg-paper rounded-lg p-3 mt-2 mb-3 gap-2 sm:mt-3 sm:mb-5 sm:p-4 overflow-auto">
       {icon && <span className="text-body1 sm:text-h3">{icon}</span>}
       <div className="flex flex-col pt-1">
         <p className="text-primary font-medium text-caption mb-0.5 sm:text-body2">
