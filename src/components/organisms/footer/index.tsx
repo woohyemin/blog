4ce2449393d44b1aa9717c08628c061ab951f6da
우혜민 <woo28870826@gmail.com>
2024-10-27 02:19:56 +0900

    💬 Footer 수정

--- src/components/organisms/footer/index.tsx
+++ src/components/organisms/footer/index.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className="w-full my-0 mx-auto px-4 max-w-[760px] pt-4 pb-8 h-20 sm:px-6 sm:pb-12 sm:h-24">
       <span className="font-Montserrat text-caption text-disabled">
-        ⓒ hyemin. All rights reserved.
+        ⓒ woohyemin. All rights reserved.
       </span>
     </footer>
   );
