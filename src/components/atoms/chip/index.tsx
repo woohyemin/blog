78eb1720be9c941235bf2b96f22208c65ddc58b5
우혜민 <woo28870826@gmail.com>
2024-10-27 02:55:31 +0900

    ✨ Chip className props 추가

--- src/components/atoms/chip/index.tsx
+++ src/components/atoms/chip/index.tsx
@@ -17,6 +17,7 @@ export interface ChipProps extends HTMLAttributes<HTMLSpanElement> {
   variant?: "contained" | "outlined";
   color?: "primary" | "secondary";
   round?: boolean;
+  className?: string;
 }
 
 /**
@@ -26,6 +27,7 @@ const Chip = ({
   variant = "contained",
   color = "secondary",
   round,
+  className,
   children,
   ...props
 }: PropsWithChildren<ChipProps>) => {
@@ -33,7 +35,7 @@ const Chip = ({
     <span
       className={`flex justify-center items-center text-caption py-0.5 px-2.5 ${
         variants[variant][color]
-      } min-w-fit h-6 ${round ? "rounded-full" : "rounded-md"}`}
+      } min-w-fit h-6 ${round ? "rounded-full" : "rounded-md"} ${className}`}
       {...props}
     >
       {children}
